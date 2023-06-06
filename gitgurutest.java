d383942597dc17aaa287bc217abe8cf4749c1b1d
Yukihiro Shinoda <52367401+sinoda1114@users.noreply.github.com>
2023-06-07 00:04:31 +0900

    Create gitgurutest.java

---
+++ gitgurutest.java
@@ -0,0 +1,11 @@
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+public class Log4jExample {
+    private static final Logger logger = LogManager.getLogger(Log4jExample.class);
+
+    public static void main(String[] args) {
+        String user_input = args[0];
+        logger.info(user_input);
+    }
+}
